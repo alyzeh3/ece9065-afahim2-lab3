a511dfed7ccefff0b0ee069343ea9f0d8372046c
alyzeh3 <afahim2@uwo.ca>
2020-11-01 08:32:12 +0500

    .gitignore should now work

---
+++ script.js
@@ -0,0 +1,42 @@
+/*fetch ('http://localhost:3000/api/subjects/sub')
+.then(res => res.json())
+.then(data =>{console.log(data)})
+.catch(error => console.log('Error'))
+*/
+
+/////////////////////
+fetch ('http://localhost:3000/api/subjects/')
+.then(res => res.text())
+.then(data =>{console.log(data)})
+.catch(error => console.log(error))
+///////////////////////
+fetch ('http://localhost:3000/api/subjects/schedule1',{
+method: 'PUT',
+headers:{
+'Content-Type': 'application/json'
+},
+body : JSON.stringify({
+name: 'AlyzehFahim'
+})
+}).then(res => {
+    return res.text()
+})
+.then(data =>{console.log(data)})
+.catch(error => console.log(error))
+
+fetch ('http://localhost:3000/api/subjects/schedule3',{
+method: 'PUT',
+headers:{
+'Content-Type': 'application/json'
+},
+body : JSON.stringify({
+name: 'AlyzehFahim',
+catalog_nbr: "54321",
+subject : "ML3032121"
+
+})
+}).then(res => {
+    return res.text()
+})
+.then(data =>{console.log(data)})
+.catch(error => console.log(error))
